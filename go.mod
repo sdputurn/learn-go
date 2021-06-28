a1dcf5daafd4bff579b82fe5680b85d009dd9cf0
Sandeep Singh <sandeep.singh03@sap.com>
2021-06-28 23:33:41 +0530

    added module

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example.com/hello
+module github.com/sdputurn/learn-go
 
 go 1.16
---
+++ hello1.go
@@ -0,0 +1,9 @@
+package learn_go
+
+import (
+	"fmt"
+)
+func Hello1() {
+	fmt.Println("i am learn_go package called by module learn-go")
+
+}
